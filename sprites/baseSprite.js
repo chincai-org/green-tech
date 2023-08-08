0a8449095bd18c8d9504701ad17a62022c57f991
Mortis66666 <mortisloverbs@gmail.com>
2023-08-08 15:41:35 +0800

    base sprite

--- sprites/baseSprite.js
+++ sprites/baseSprite.js
@@ -1,5 +1,29 @@
 class BaseSprite {
     constructor(config) {
         this.config = config;
+
+        ({
+            id: this.id,
+            x: this.x,
+            y: this.y,
+            hp: this.hp,
+            damage: this.damage,
+            speed: this.speed,
+            cost: this.cost
+        } = config);
+    }
+
+    update() {
+        _update();
+    }
+
+    move() {
+        _move();
+    }
+
+    _update() {}
+
+    _move(deltaTime) {
+        // TODO: base move
     }
 }

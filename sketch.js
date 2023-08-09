a41c4bd8f76842025550304e9a8aadba839c1269
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2023-08-09 16:53:54 +0800

    fix canva overflowing i think

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
+body = document.getElementById("body");
+
 function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.style("position", "fixed");
+    canvas.style("top", "0");
+    canvas.style("left", "0");
+    canvas.style("z-index", "-1");
     canvas.parent("body");
 
     background(0);

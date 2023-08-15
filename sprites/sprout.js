87db75031e7c2d4e527dee108f7184394d6ca174
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2023-08-15 16:08:53 +0800

    local stuff

--- sprites/sprout.js
+++ sprites/sprout.js
@@ -1,14 +1,13 @@
-let UP;
-let RIGHT;
-let LEFT;
-let DOWN;
-
 class Sprout extends BaseSprite {
     constructor(x, y) {
         super({ x: (window.innerWidth/2), y: (window.innerHeight/2), color: 256, speed: 0.2 });
     }
 
     move(deltaTime) {
+        let UP;
+        let RIGHT;
+        let LEFT;
+        let DOWN;
         UP = keyIsDown(UP_ARROW) || keyIsDown(87)
         RIGHT = keyIsDown(RIGHT_ARROW) || keyIsDown(68)
         LEFT = keyIsDown(LEFT_ARROW) || keyIsDown(65)

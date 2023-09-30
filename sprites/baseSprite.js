4ec607680b2a3a172866d0e3dcfe6ad89a02541b
Lee Hoon Lim <hoonlim123@gmail.com>
2023-09-30 22:07:28 +0800

    fix lumberjack undefined img bug

--- sprites/baseSprite.js
+++ sprites/baseSprite.js
@@ -44,7 +44,7 @@ class BaseSprite {
         let distance = this.distance({ x: camX, y: camY });
         let drawX = windowWidth / 2 + distance.x;
         let drawY = windowHeight / 2 + distance.y;
-        if (this.config.img === null) {
+        if (this.config.img === null || this.config.img === undefined) {
             fill(this.config.color);
             circle(drawX, drawY, 13 * widthRatio);
         } else {
